e5a485b3e301e3c31f29bce09aa6a062fe735fd0
Pieter Staal <pietdagamer@gmail.com>
2016-09-16 16:48:49 +0200

    Add functionality

--- mm.py
+++ mm.py
@@ -1,36 +1,53 @@
 import praw
 import time
+import os.path
 
-def log(submission, word):
-    print "Word: \n    ", word
-    print "Title: \n    ", submission.title
-    print "Text: \n    ", submission.selftext
-    print "Link: \n    ", submission.permalink
+def log(submission, word, prices):
+    file_name = submission.url.split("/")[6]
+    print file_name
+    if not os.path.isfile(file_name):
+        print "Writing"
+        file = open(file_name, 'w')
+        file.write("Word found: " + word)
+        file.write("\nFull Title: " + submission.title)
+        file.write("\nURL to post: " + submission.url)
+        file.write("\nPrices found:")
+        for p in prices:
+            file.write(p + "$")
 
-r = praw.Reddit(user_agent='my_cool_application')
+        file.close()
 
-words = ["SA", "DSA", "gmk", "Carbon", "Hydro", "CM"]
+
+r = praw.Reddit(user_agent='Mech market logger for market research in mechanical keyboards')
+
+words = ["SA", "DSA", "gmk", "Carbon", "Hydro", "a"]
 words = [x.lower() for x in words]
-print words
 
-for submission in r.get_subreddit('mechmarket').get_new(limit=1):
-    time.sleep(0)
+while True:
+    for submission in r.get_subreddit('mechmarket').get_new(limit=1):
+        time.sleep(5)
+        print submission.title
 
-    # modify the raw data so it is easier to search in
+        # modify the raw data so it is easier to search in
 
-    title = submission.title.lower()
-    text = submission.selftext.lower()
-    try:
-        title = title.split("[h]")[1].split("[w]")[0]
-    except:
-        continue
-
-    # check if words of interest are in the title after [H] but before [W]
-    for i in range(0, len(words)):
-        if words[i] in title:
-            log(submission, words[i])
-
-    # check if some kind of price is mentioned in the text
+        title = submission.title.lower()
+        text = submission.selftext.lower()
+        try:
+            title = title.split("[h]")[1].split("[w]")[0]
+        except:
+            continue
 
 
-    # log findings to a text file
+        # check if some kind of price is mentioned in the text
+        price_found = False
+        prices = []
+        if "$" in text:
+            splittext = text.split("$")
+            prices.append(splittext[1].split(" ")[0])
+            price = splittext[0].split(" ")
+            prices.append(price[len(price) - 1])
+
+        # check if words of interest are in the title after [H] but before [W]
+        for i in range(0, len(words)):
+            if words[i] in title:
+                log(submission, words[i], prices)
