8471ee6d7b38878958bc05abd9db547a2718c854
Pieter Staal <pietdagamer@gmail.com>
2016-09-19 10:48:13 +0200

    .

--- mm.py
+++ mm.py
@@ -1,6 +1,7 @@
 import praw
 import time
 import os.path
+import sys
 
 def log(submission, word, prices):
     file = open(file_name, 'w')
@@ -26,39 +27,42 @@ words = [x.lower() for x in words]
 while True:
     print "Scanning..."
     time.sleep(16)
+
+    submission = 0
     try:
-        submission = r.get_subreddit('mechmarket').get_new(limit=1)[0]
+        for s in r.get_subreddit('mechmarket').get_new(limit=1):
+            submission = s
     except:
         continue
 
 
-        # modify the raw data so it is easier to search in
+    # modify the raw data so it is easier to search in
 
-        title = submission.title.lower()
-        text = submission.selftext.lower()
+    title = submission.title.lower()
+    text = submission.selftext.lower()
+    print title
+    try:
+        title = title.split("[h]")[1].split("[w]")[0]
+        title = title.replace(",","").replace(".","")
         print title
-        try:
-            title = title.split("[h]")[1].split("[w]")[0]
-            title = title.replace(",","").replace(".","")
-            print title
-        except:
-            continue
+    except:
+        continue
 
 
-        # check if some kind of price is mentioned in the text
-        price_found = False
-        prices = []
-        if "$" in text:
-            splittext = text.split("$")
-            prices.append(splittext[1].split(" ")[0])
-            price = splittext[0].split(" ")
-            prices.append(price[len(price) - 1])
+    # check if some kind of price is mentioned in the text
+    price_found = False
+    prices = []
+    if "$" in text:
+        splittext = text.split("$")
+        prices.append(splittext[1].split(" ")[0])
+        price = splittext[0].split(" ")
+        prices.append(price[len(price) - 1])
 
 
-        file_name = "./log/" + submission.url.split("/")[6]
+    file_name = "./log/" + submission.url.split("/")[6]
 
-        # check if words of interest are in the title after [H] but before [W]
-        for i in range(0, len(words)):
-            word = " " + words[i] + " "
-            if word in title and not os.path.isfile(file_name):
-                log(submission, words[i], prices)
+    # check if words of interest are in the title after [H] but before [W]
+    for i in range(0, len(words)):
+        word = " " + words[i] + " "
+        if word in title and not os.path.isfile(file_name):
+            log(submission, words[i], prices)
